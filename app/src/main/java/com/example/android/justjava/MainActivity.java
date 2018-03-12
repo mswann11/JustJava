a404a05071ad3a94517af41e454822469573613f
mswann11 <mswann11@gmail.com>
2018-03-12 19:59:45 -0400

    Update with Spanish language version

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -16,22 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
+    int quantity = 1;
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        quantity++;
-        displayQuantity(quantity);
+        if(quantity<10) {
+            quantity++;
+            displayQuantity(quantity);
+        }
+        else{
+            CharSequence text = "You cannot order more than 10 coffees.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }
     }
 
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        quantity--;
-        displayQuantity(quantity);
+        if(quantity>1) {
+            quantity--;
+            displayQuantity(quantity);
+        }
+        else{
+            CharSequence text = "You cannot order less than 1 coffee.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }
     }
 
     /**
@@ -45,21 +64,36 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method calculates the total price.
+     * @param hasWhipped is true if the customer requests whipped cream
+     * @param hasChocolate is true if the customer requests chocolate
+     * @return total price
      */
-    private int calculatePrice(){
-        return quantity * 5;
+    private int calculatePrice(boolean hasWhipped, boolean hasChocolate){
+        int pricePerCup = 5;
+        if (hasWhipped){
+            pricePerCup += 1;
+        }
+        if (hasChocolate){
+            pricePerCup += 2;
+        }
+        return quantity * pricePerCup;
     }
 
     /**
      * This method creates a String that shows the order display.
+     * @param name is customer name
      * @param price of the order
+     * @param hasWhippedCream indicates if the user wants whipped cream
+     * @param hasChocolate indicates if the user wants chocolate
      * @return text summary
      */
-    private String createOrderSummary(int price){
-        String orderSummary = "Name: Marianne";
-        orderSummary += "\nQuantity: " + quantity;
-        orderSummary += "\nTotal: $" + price;
-        orderSummary += "\nThank you!";
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate){
+        String orderSummary = getString(R.string.name_summary, name);
+        orderSummary += "\n" + getString(R.string.whipped_summary) + hasWhippedCream;
+        orderSummary += "\n" + getString(R.string.chocolate_summary) + hasChocolate;
+        orderSummary += "\n" + getString(R.string.quantity_summary) + quantity;
+        orderSummary += "\n" + getString(R.string.total) + price;
+        orderSummary += "\n" + getString(R.string.thank_you);
         return orderSummary;
     }
 
@@ -67,7 +101,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(createOrderSummary(calculatePrice()));
+        CheckBox checkBoxWhipped = (CheckBox) findViewById(R.id.whipped_cream_check);
+        CheckBox checkBoxChocolate = (CheckBox) findViewById(R.id.chocolate_check);
+        EditText nameTextView = (EditText) findViewById(R.id.name);
+        String name = nameTextView.getText().toString();
+        boolean hasWhipped = checkBoxWhipped.isChecked();
+        boolean hasChocolate = checkBoxChocolate.isChecked();
+        int totalPrice = calculatePrice(hasWhipped, hasChocolate);
+        String priceMessage = createOrderSummary(name, totalPrice, hasWhipped, hasChocolate);
+        displayMessage(priceMessage);
+
+//        Intent intent = new Intent(Intent.ACTION_SENDTO);
+//        intent.setData(Uri.parse("mailto:"));
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for " + name);
+//        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
     }
 
     /**
